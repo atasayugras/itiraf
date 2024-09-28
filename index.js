d76abbfc99bbe29c483906fb7ba24af5caea32c0
Atasay Ugras <atasayugras@gmail.com>
2024-09-28 12:21:23 +0300

    Minor changes

--- index.js
+++ index.js
@@ -1,15 +1,14 @@
 import express from 'express';
 import axios from 'axios';
 import dotenv from 'dotenv';
-import ejs from 'ejs';
 
 dotenv.config();
 
 const app = express();
 app.set('view engine', 'ejs');
+app.use(express.static('public'));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-app.use(express.static('public'));
 
 const PORT = 3000;
 const API_URL = process.env.API_URL;
@@ -18,7 +17,7 @@ const ACCESS_KEY = process.env.ACCESS_KEY;
 
 //Render form page
 app.get(`/post`, (req, res) => {
-  res.render('post.ejs');
+  res.render('post');
 });
 
 //Get 1 random post from array inside the bin
@@ -40,7 +39,7 @@ app.get(`/`, async (req, res) => {
         res.render('index', {data: randomPost});
       } catch (error) {
         console.log(error);
-        res.render('index.ejs', { error: error.response ? error.response.data : 'An error occurred' });
+        res.render('index', { error: error.response ? error.response.data : 'An error occurred' });
       };
 });
 
@@ -65,7 +64,7 @@ app.post(`/post`, async (req, res) => {
       const updatedPosts = [...existingPosts.data.record.posts, newPost];
       
       //Send the updated posts back to the API
-      const updatePosts = await axios.put(`${API_URL}`, {
+      await axios.put(`${API_URL}`, {
         posts: updatedPosts,
       }, {
         headers: {
